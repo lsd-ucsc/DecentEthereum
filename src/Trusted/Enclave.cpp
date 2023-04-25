95fc869dd9641339ed95c943e51c212f55775833
Haofan Zheng <hzheng6@ucsc.edu>
2023-04-25 00:14:31 -0700

    Added enclave impl

--- src/Trusted/Enclave.cpp
+++ src/Trusted/Enclave.cpp
@@ -3,11 +3,241 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+
+#include <sgx_edger8r.h>
+
 #include <DecentEnclave/Common/Platform/Print.hpp>
+#include <DecentEnclave/Common/Sgx/MbedTlsInit.hpp>
 
-using namespace DecentEnclave::Common;
+#include <DecentEnclave/Trusted/DecentLambdaSvr.hpp>
+#include <DecentEnclave/Trusted/PlatformId.hpp>
+#include <DecentEnclave/Trusted/SKeyring.hpp>
+#include <DecentEnclave/Trusted/Sgx/EnclaveIdentity.hpp>
+#include <DecentEnclave/Trusted/AppCertRequester.hpp>
+#include <DecentEnclave/Trusted/DecentLambdaSvr.hpp>
 
-extern "C" void ecall_decent_ethereum_init()
+#include <DecentEthereum/Trusted/BlockchainMgr.hpp>
+#include <DecentEthereum/Trusted/Pubsub/SubscriberHandler.hpp>
+
+#include <EclipseMonitor/MonitorReport.hpp>
+
+#include "Certs.hpp"
+#include "Keys.hpp"
+
+
+using EthChainConfig = EclipseMonitor::Eth::GoerliConfig;
+
+
+namespace DecentEthereum
 {
-	Platform::Print::Str("Hello, world!\n");
+
+
+std::shared_ptr<Trusted::BlockchainMgr<EthChainConfig> >
+	g_blockchainMgr;
+
+
+void GlobalInitialization()
+{
+	using namespace DecentEnclave::Common;
+
+	// Initialize mbedTLS
+	Sgx::MbedTlsInit::Init();
+
+	using namespace DecentEnclave::Trusted;
+	// Register keys
+	DecentKey_Secp256r1::Register();
+	DecentKey_Secp256k1::Register();
+
+	// Register certificates
+	DecentCert_Secp256r1::Register();
+	DecentCert_Secp256k1::Register();
+}
+
+
+void PrintMyInfo()
+{
+	using namespace DecentEnclave::Common;
+	using namespace DecentEnclave::Trusted;
+
+	Platform::Print::StrInfo(
+		"My platform ID is              : " + PlatformId::GetIdHex()
+	);
+
+	const auto& selfHash = DecentEnclave::Trusted::Sgx::EnclaveIdentity::GetSelfHashHex();
+	Platform::Print::StrInfo(
+		"My enclave hash is             : " + selfHash
+	);
+
+	std::string secp256r1KeyFp =
+		DecentKey_Secp256r1::GetInstance().GetKeySha256Hex();
+	std::string secp256k1KeyFp =
+		DecentKey_Secp256k1::GetInstance().GetKeySha256Hex();
+	std::string keyringHash = Keyring::GetInstance().GenHashHex();
+	Platform::Print::StrInfo(
+		"My key fingerprint (SECP256R1) : " + secp256r1KeyFp
+	);
+	Platform::Print::StrInfo(
+		"My key fingerprint (SECP256K1) : " + secp256k1KeyFp
+	);
+	Platform::Print::StrInfo(
+		"My keyring hash is             : " + keyringHash
+	);
+}
+
+
+template<typename _CertStoreCertType>
+inline void RequestAppCert(const std::string& keyName)
+{
+	std::string pemChain = DecentEnclave::Trusted::AppCertRequester(
+		"DecentServer",
+		keyName
+	).Request();
+
+	auto cert = std::make_shared<mbedTLScpp::X509Cert>(
+		mbedTLScpp::X509Cert::FromPEM(pemChain)
+	);
+
+	_CertStoreCertType::Update(cert);
+}
+
+
+inline void HandleSubscribeRequest(
+	DecentEnclave::Trusted::LambdaHandlerMgr::SocketPtrType& socket,
+	const DecentEnclave::Trusted::LambdaHandlerMgr::MsgIdExtType& msgIdExt,
+	const DecentEnclave::Trusted::LambdaHandlerMgr::MsgContentType& msgContent
+)
+{
+	Trusted::Pubsub::HandleSubscribeRequest(
+		g_blockchainMgr,
+		socket,
+		msgIdExt,
+		msgContent
+	);
+}
+
+
+void Init(
+	const EclipseMonitor::MonitorConfig& mConf,
+	EclipseMonitor::Eth::BlockNumber startBlkNum,
+	const EclipseMonitor::Eth::ContractAddr& syncContractAddr,
+	const std::string& syncEventSign,
+	std::unique_ptr<Trusted::HostBlockService> blkSvc
+)
+{
+	using namespace DecentEnclave::Trusted;
+
+	GlobalInitialization();
+	PrintMyInfo();
+
+	g_blockchainMgr = std::make_shared<Trusted::BlockchainMgr<EthChainConfig> >(
+		mConf,
+		startBlkNum,
+		syncContractAddr,
+		syncEventSign,
+		SimpleObjects::Internal::
+			make_unique<Trusted::Pubsub::SubscriberService>(
+				// Pubsub Addr: 0x5651231eA05C0478f60c13a7f5FE291657012C86
+				EclipseMonitor::Eth::ContractAddr({
+					0x56U, 0x51U, 0x23U, 0x1eU, 0xa0U, 0x5cU, 0x04U, 0x78U, 0xf6U, 0x0cU,
+					0x13U, 0xa7U, 0xf5U, 0xfeU, 0x29U, 0x16U, 0x57U, 0x01U, 0x2cU, 0x86U,
+				}),
+				"ServiceDeployed(address)",
+				"PublisherRegistered(address,address)",
+				"NotifySubscribers(bytes)"
+		),
+		std::move(blkSvc)
+	);
+
+	RequestAppCert<DecentCert_Secp256r1>("Secp256r1");
+	RequestAppCert<DecentCert_Secp256k1>("Secp256k1");
+
+	LambdaServerConfig lambdaSvrConfig(
+		"Secp256r1",
+		"Secp256r1"
+	);
+	LambdaServerConfig::GetInstance(&lambdaSvrConfig);
+
+	LambdaHandlerMgr::GetInstance().RegisterHandler(
+		"Subscribe",
+		HandleSubscribeRequest
+	);
+}
+
+
+void RecvBlock(const std::vector<uint8_t>& hdrRlp)
+{
+	g_blockchainMgr->AppendBlock(hdrRlp);
+}
+
+
+} // namespace DecentEthereum
+
+
+extern "C" sgx_status_t ecall_decent_ethereum_init(
+	const uint8_t* in_conf,
+	size_t in_conf_size,
+	uint64_t start_blk_num,
+	const uint8_t* in_sync_addr,
+	const char* in_sync_esign,
+	void* host_blk_svc
+)
+{
+	using namespace DecentEthereum;
+
+	try
+	{
+		std::vector<uint8_t> mConfAdvRlp(in_conf, in_conf + in_conf_size);
+
+		EclipseMonitor::MonitorConfig mConf =
+			EclipseMonitor::MonitorConfigParser().Parse(mConfAdvRlp);
+
+		EclipseMonitor::Eth::ContractAddr syncContractAddr;
+		std::copy(
+			in_sync_addr,
+			in_sync_addr + syncContractAddr.size(),
+			syncContractAddr.begin()
+		);
+
+		std::string syncEventSign(in_sync_esign);
+
+		std::unique_ptr<Trusted::HostBlockService> blkSvc =
+			SimpleObjects::Internal::
+				make_unique<Trusted::HostBlockService>(host_blk_svc);
+
+		DecentEthereum::Init(
+			mConf,
+			start_blk_num,
+			syncContractAddr,
+			syncEventSign,
+			std::move(blkSvc)
+		);
+		return SGX_SUCCESS;
+	}
+	catch(const std::exception& e)
+	{
+		using namespace DecentEnclave::Common;
+		Platform::Print::StrErr(e.what());
+		return SGX_ERROR_UNEXPECTED;
+	}
+}
+
+
+extern "C" sgx_status_t ecall_decent_ethereum_recv_block(
+	const uint8_t* hdr_rlp,
+	size_t hdr_size
+)
+{
+	try
+	{
+		std::vector<uint8_t> hdrRlp(hdr_rlp, hdr_rlp + hdr_size);
+		DecentEthereum::RecvBlock(hdrRlp);
+
+		return SGX_SUCCESS;
+	}
+	catch(const std::exception& e)
+	{
+		using namespace DecentEnclave::Common;
+		Platform::Print::StrErr(e.what());
+		return SGX_ERROR_UNEXPECTED;
+	}
 }
--- src/Untrusted/Main.cpp
+++ src/Untrusted/Main.cpp
@@ -3,16 +3,236 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-#include "DecentEthereumEnclave.hpp"
 
+#include <memory>
+#include <vector>
+
+#include <DecentEnclave/Common/Platform/Print.hpp>
+#include <DecentEnclave/Common/Sgx/MbedTlsInit.hpp>
+
+#include <DecentEnclave/Untrusted/Config/AuthList.hpp>
+#include <DecentEnclave/Untrusted/Config/EndpointsMgr.hpp>
+#include <DecentEnclave/Untrusted/Hosting/BoostAsioService.hpp>
+#include <DecentEnclave/Untrusted/Hosting/HeartbeatEmitterService.hpp>
+#include <DecentEnclave/Untrusted/Hosting/LambdaFuncServer.hpp>
+
+#include <DecentEthereum/Untrusted/HostBlockService.hpp>
+#include <DecentEthereum/Untrusted/DecentEthereumEnclave.hpp>
+
+#include <SimpleConcurrency/Threading/ThreadPool.hpp>
+#include <SimpleJson/SimpleJson.hpp>
+#include <SimpleRlp/SimpleRlp.hpp>
+#include <SimpleObjects/Internal/make_unique.hpp>
+#include <SimpleObjects/SimpleObjects.hpp>
+#include <SimpleSysIO/SysCall/Files.hpp>
+
+#include "RunUntilSignal.hpp"
+
+
+using namespace DecentEnclave;
+using namespace DecentEnclave::Common;
+using namespace DecentEnclave::Untrusted;
 using namespace DecentEthereum;
+using namespace SimpleConcurrency::Threading;
+using namespace SimpleSysIO::SysCall;
 
-int main(int argc, char* argv[]) {
+
+std::shared_ptr<ThreadPool> GetThreadPool()
+{
+	static  std::shared_ptr<ThreadPool> threadPool =
+		std::make_shared<ThreadPool>(5);
+
+	return threadPool;
+}
+
+
+static void StartSendingBlocks(
+	HostBlockService& blkSvc,
+	uint64_t startBlockNum
+)
+{
+	if (blkSvc.GetCurrBlockNum() != 0)
+	{
+		throw std::runtime_error("Block update service already started.");
+	}
+
+	blkSvc.SetUpdSvcStartBlock(startBlockNum);
+	auto blkSvcSPtr = blkSvc.GetSharedPtr();
+
+	auto blkUpdStatusSvc = std::unique_ptr<HostBlockStatusLogTask>(
+		new HostBlockStatusLogTask(blkSvcSPtr, 10 * 1000)
+	);
+	auto blkUpdSvc = std::unique_ptr<BlockUpdatorServiceTask>(
+		new BlockUpdatorServiceTask(blkSvcSPtr, 1 * 1000)
+	);
+
+	std::shared_ptr<ThreadPool> threadPool = GetThreadPool();
+
+	threadPool->AddTask(std::move(blkUpdStatusSvc));
+	threadPool->AddTask(std::move(blkUpdSvc));
+}
+
+
+int main(int argc, char* argv[])
+{
 	(void)argc;
 	(void)argv;
 
-	DecentEthereumEnclave enclave;
-	enclave.Init();
+	// Init MbedTLS
+	Common::Sgx::MbedTlsInit::Init();
+
+
+	// Thread pool
+	std::shared_ptr<ThreadPool> threadPool = GetThreadPool();
+
+
+	// Read in components config
+	auto configFile = RBinaryFile::Open(
+		"../../src/components_config.json"
+	);
+	auto configJson = configFile->ReadBytes<std::string>();
+	auto config = SimpleJson::LoadStr(configJson);
+	std::vector<uint8_t> authListAdvRlp = Config::ConfigToAuthListAdvRlp(config);
+
+
+	// Boost IO Service
+	std::unique_ptr<Hosting::BoostAsioService> asioService =
+		SimpleObjects::Internal::make_unique<Hosting::BoostAsioService>();
+
+
+	// Endpoints Manager
+	auto endpointMgr = Config::EndpointsMgr::GetInstancePtr(
+		&config,
+		asioService->GetIoService()
+	);
+
+
+	// Host block service
+	std::shared_ptr<HostBlockService> hostBlkSvc = HostBlockService::Create(
+		"http://localhost:8545"
+	);
+		// std::make_shared<HostBlockService>(
+		// 	8620000, "http://localhost:8545"
+		// );
+
+
+	// Sync Event Contract Address
+	const EclipseMonitor::Eth::ContractAddr decentSyncV2Addr = {
+		0X74U, 0XBEU, 0X86U, 0X7FU, 0XBDU, 0X89U, 0XBCU, 0X35U,
+		0X07U, 0XF1U, 0X45U, 0XB3U, 0X6BU, 0XA7U, 0X6CU, 0XD0U,
+		0XB1U, 0XBFU, 0X4FU, 0X1AU,
+	};
+
+	// Enclave
+	// uint64_t startBlockNum = 8620000;
+	// uint64_t startBlockNum = 0;
+	// uint64_t startBlockNum = 8814199;
+	uint64_t startBlockNum = 8875000;
+	std::shared_ptr<DecentEthereumEnclave> enclave =
+		std::make_shared<DecentEthereumEnclave>(
+			EclipseMonitor::BuildEthereumMonitorConfig(),
+			startBlockNum,
+			decentSyncV2Addr,
+			"SyncMsg(bytes16,bytes32)",
+			hostBlkSvc,
+			authListAdvRlp
+		);
+	hostBlkSvc->BindReceiver(enclave);
+	StartSendingBlocks(*hostBlkSvc, startBlockNum);
+
+
+	// API call server
+	Hosting::LambdaFuncServer lambdaFuncSvr(
+		endpointMgr,
+		threadPool
+	);
+	// Setup Lambda call handlers and start to run multi-threaded-ly
+	lambdaFuncSvr.AddFunction("DecentEthereum", enclave);
+
+
+	// Heartbeat emitter
+	auto heartbeatEmitter = std::unique_ptr<Hosting::HeartbeatEmitterService>(
+		new Hosting::HeartbeatEmitterService(enclave, 2000)
+	);
+	threadPool->AddTask(std::move(heartbeatEmitter));
+
+
+	// Start IO service
+	threadPool->AddTask(std::move(asioService));
+
+
+	RunUntilSignal(
+		[&]()
+		{
+			threadPool->Update();
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+	);
+
+
+	threadPool->Terminate();
+
 
 	return 0;
+}
+
+
+extern "C" sgx_status_t ocall_decent_ethereum_get_receipts(
+	const void* host_blk_svc,
+	uint64_t blk_num,
+	uint8_t** out_buf,
+	size_t* out_buf_size
+)
+{
+	using _ListBytesType = SimpleObjects::ListT<SimpleObjects::Bytes>;
+
+	const HostBlockService* blkSvc =
+		static_cast<const HostBlockService*>(host_blk_svc);
+
+	try
+	{
+		_ListBytesType listOfReceipts =
+			blkSvc->GetReceiptsRlpByNum<_ListBytesType>(blk_num);
+
+		std::vector<uint8_t> bytes = SimpleRlp::WriteRlp(listOfReceipts);
+
+		*out_buf = new uint8_t[bytes.size()];
+		*out_buf_size = bytes.size();
+
+		std::copy(bytes.begin(), bytes.end(), *out_buf);
+
+		return SGX_SUCCESS;
+	}
+	catch (const std::exception& e)
+	{
+		DecentEnclave::Common::Platform::Print::StrDebug(
+			"ocall_decent_ethereum_get_receipts failed with error " +
+			std::string(e.what())
+		);
+		return SGX_ERROR_UNEXPECTED;
+	}
+}
+
+extern "C" sgx_status_t ocall_decent_ethereum_get_latest_blknum(
+	const void* host_blk_svc,
+	uint64_t* out_blk_num
+)
+{
+	const HostBlockService* blkSvc =
+		static_cast<const HostBlockService*>(host_blk_svc);
+
+	try
+	{
+		*out_blk_num = blkSvc->GetLatestBlockNum();
+
+		return SGX_SUCCESS;
+	}
+	catch (const std::exception& e)
+	{
+		DecentEnclave::Common::Platform::Print::StrDebug(
+			"ocall_decent_ethereum_get_latest_blknum failed with error " +
+			std::string(e.what())
+		);
+		return SGX_ERROR_UNEXPECTED;
+	}
 }
